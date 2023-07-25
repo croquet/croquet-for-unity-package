6959fee2960ce2421c992e53231dcbf3627b24c1
Aran Lunzer <aran@acm.org>
2023-07-25 11:50:04 -0700

    typo in transform update

--- Scripts/Runtime/Systems/CroquetSpatialSystem.cs
+++ Scripts/Runtime/Systems/CroquetSpatialSystem.cs
@@ -353,7 +353,7 @@ public class CroquetSpatialSystem : CroquetSystem
 
         if (scale != null)
         {
-            argList.Add("p");
+            argList.Add("s");
             argList.Add(string.Join<float>(",", new[] { scale.Value.x, scale.Value.y, scale.Value.z }));
         }
 
